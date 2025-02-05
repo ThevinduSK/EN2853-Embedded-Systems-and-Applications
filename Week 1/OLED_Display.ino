166ae76341606ff9f36bdf6d08d8642d083c5c4f
Thevindu Kalubowila <kalubowilakats.22@uom.lk>
2025-02-05 16:06:42 +0530

    Update OLED_Display.ino

---
+++ Week 1/OLED_Display.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire , OLED_RESET);
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  // Initialize serial monitor and OLED display
+  Serial.begin(115200);
+  if (! display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)){
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+
+  //turn on OLED display
+  display.display();
+  delay(2000);
+
+  //clear OLED display
+  display.clearDisplay();
+
+  //display a custom message
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println(F("Welcome to Medibox!"));
+  display.display();
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10); // this speeds up the simulation
+}
