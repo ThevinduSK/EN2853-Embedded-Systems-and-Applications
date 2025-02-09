014dabb4b3d807d0d741910c3436c4adbb210fde
Thevindu Kalubowila <kalubowilakats.22@uom.lk>
2025-02-09 18:55:44 +0530

    alarm function

--- Week 2/Screen.ino
+++ Week 2/Screen.ino
@@ -7,6 +7,9 @@
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
 
+#define BUZZER 5
+#define LED_1 15
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire , OLED_RESET);
 
 // Global variables
@@ -18,7 +21,28 @@ int seconds = 0;
 unsigned long timeNow = 0;
 unsigned long timeLast = 0;
 
+bool alarm_enabled = true;
+int n_alarms = 2;
+int alarm_hours[] = {0,1};
+int alarm_minutes[] = {1,10};
+bool alarm_triggered[] = {false, false};
+
+int n_notes = 8;
+int C = 262;
+int D = 294;
+int E = 330;
+int F = 349;
+int G = 392;
+int A = 440;
+int B = 494;
+int C_H = 523;
+int notes[] = {C, D, E, F, G, A, B, C_H};
+
 void setup() {
+
+  pinMode(BUZZER, OUTPUT);
+  pinMode(LED_1, OUTPUT);
+
   Serial.begin(9600);
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -27,17 +51,16 @@ void setup() {
   }
 
   display.display();
-  delay(2000);
+  delay(500);
   display.clearDisplay();
 
   print_line("Welcome to Medibox!", 10, 20, 2);
-
+  delay(500);
   display.clearDisplay();
 }
 
 void loop() {
-  update_time();
-  print_time_now();
+  update_time_with_check_alarm();
 }
 
 void print_line(String text, int column, int row, int text_size) {
@@ -78,5 +101,35 @@ void update_time() {
   if (hours == 24) {
     days += 1;
     hours = 0;
+  }
+}
+void ring_alarm(){
+  display.clearDisplay();
+  print_line("MEDICINE TIME!", 0, 0, 2);
+
+  digitalWrite(LED_1, HIGH);
+
+  //ring the buzzer
+  for (int i=0; i<n_notes; i++){
+    tone(BUZZER, notes[i]);
+    delay(500);
+    noTone(BUZZER);
+    delay(2);
+  }
+
+  digitalWrite(LED_1, LOW);
+  display.clearDisplay();
+}
+
+void update_time_with_check_alarm(void){
+  update_time();
+  print_time_now();
+
+  if (alarm_enabled == true){
+    for (int i=0; i<n_alarms; i++){
+      if (alarm_triggered[i] == false && alarm_hours[i] == hours && alarm_minutes[i] == minutes){
+        ring_alarm();
+      }
+    }
   }
 }
