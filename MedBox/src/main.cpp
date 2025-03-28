fd024ec101c78a21e4c8473a97c8d9ba1fc555b0
Thevindu Kalubowila <thevindukalubowila123@gmail.com>
2025-03-28 09:39:46 +0530

    Set timezone function

--- MedBox/src/main.cpp
+++ MedBox/src/main.cpp
@@ -18,31 +18,25 @@
 #define DHTPIN 12
 
 #define NTP_SERVER     "pool.ntp.org"
-#define UTC_OFFSET     0
 #define UTC_OFFSET_DST 0
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire , OLED_RESET);
 DHTesp dhtSensor;
 
-// Function declarations
-void print_line(String text, int column, int row, int text_size);
-void update_time_with_check_alarm(void);
-void go_to_menu();
-void check_temp();
-void run_mode(int mode);
-
+// Global variables
 int days = 0;
 int hours = 0;
 int minutes = 0;
 int seconds = 0;
+float UTC_OFFSET = 0.0; 
 
 unsigned long timeNow = 0;
 unsigned long timeLast = 0;
 
 bool alarm_enabled = true;
 int n_alarms = 2;
-int alarm_hours[] = {0, 1};
-int alarm_minutes[] = {1, 10};
+int alarm_hours[] = {0,1};
+int alarm_minutes[] = {1,10};
 bool alarm_triggered[] = {false, false};
 
 int n_notes = 8;
@@ -57,10 +51,24 @@ int C_H = 523;
 int notes[] = {C, D, E, F, G, A, B, C_H};
 
 int current_mode = 0;
-int max_modes = 4;
-String modes[] = {"1 - Set Time", "2 - Set Alarm 1", "3 - Set Alarm 2", "4 - Disable Alarms"};
+int max_modes = 7;
+String modes[] = { "1 - Set Time",
+                   "2 - Set Alarm 1", 
+                   "3 - Set Alarm 2", 
+                   "4 - Disable Alarms", 
+                   "5 - Set Timezone", 
+                   "6 - View Alarms", 
+                   "7 - Delete Alarm"};
+
+void print_line(String text, int column, int row, int text_size);
+void update_time_with_check_alarm(void);
+void go_to_menu();
+void check_temp();
+void run_mode(int mode);
+void set_timezone();
 
 void setup() {
+
   pinMode(BUZZER, OUTPUT);
   pinMode(LED_1, OUTPUT);
   pinMode(PB_CANCEL, INPUT);
@@ -90,7 +98,7 @@ void setup() {
   display.clearDisplay();
   print_line("Connected to WIFI", 0, 0, 2);
 
-  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
+  configTime((int)(UTC_OFFSET * 3600), UTC_OFFSET_DST, NTP_SERVER);
 
   display.clearDisplay();
   display.display();
@@ -114,11 +122,13 @@ void print_line(String text, int column, int row, int text_size) {
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(column, row);
   display.println(text); 
+
   display.display();
 }
 
 void print_time_now(void) {
-  display.clearDisplay();  
+  display.clearDisplay();  // 
+
   print_line(String(days), 0, 0, 2);
   print_line(":", 20, 0, 2);
   print_line(String(hours), 30, 0, 2);
@@ -130,32 +140,29 @@ void print_time_now(void) {
 
 void update_time() {
   struct tm timeinfo;
-  getLocalTime(&timeinfo);
-
-  char timeHour[3], timeMinute[3], timeSecond[3], timeDay[3];
-
-  strftime(timeHour, 3, "%H", &timeinfo);
-  strftime(timeMinute, 3, "%M", &timeinfo);
-  strftime(timeSecond, 3, "%S", &timeinfo);
-  strftime(timeDay, 3, "%d", &timeinfo);
-
-  hours = atoi(timeHour);
-  minutes = atoi(timeMinute);
-  seconds = atoi(timeSecond);
-  days = atoi(timeDay);  
+  if (getLocalTime(&timeinfo)) {
+    hours = timeinfo.tm_hour;
+    minutes = timeinfo.tm_min;
+    seconds = timeinfo.tm_sec;
+    days = timeinfo.tm_mday;
+  } else {
+    Serial.println("Failed to get time from NTP.");
+  }
 }
 
 void ring_alarm(){
   display.clearDisplay();
   print_line("MEDICINE TIME!", 0, 0, 2);
+
   digitalWrite(LED_1, HIGH);
 
   bool break_happened = false;
-  while (!break_happened && digitalRead(PB_CANCEL) == HIGH){
-    for (int i = 0; i < n_notes; i++) {
+
+  //ring the buzzer
+  while (break_happened == false && digitalRead(PB_CANCEL) == HIGH){
+    for (int i=0; i<n_notes; i++){
       if (digitalRead(PB_CANCEL) == LOW){
         delay(200);
-        break_happened = true;
         break;
       }
       tone(BUZZER, notes[i]);
@@ -164,6 +171,7 @@ void ring_alarm(){
       delay(2);
     }
   }
+    
 
   digitalWrite(LED_1, LOW);
   display.clearDisplay();
@@ -173,9 +181,9 @@ void update_time_with_check_alarm(void){
   update_time();
   print_time_now();
 
-  if (alarm_enabled){
-    for (int i = 0; i < n_alarms; i++){
-      if (!alarm_triggered[i] && alarm_hours[i] == hours && alarm_minutes[i] == minutes){
+  if (alarm_enabled == true){
+    for (int i=0; i<n_alarms; i++){
+      if (alarm_triggered[i] == false && alarm_hours[i] == hours && alarm_minutes[i] == minutes){
         ring_alarm();
         alarm_triggered[i] = true;
       }
@@ -188,16 +196,23 @@ int wait_for_button_press(){
     if (digitalRead(PB_UP) == LOW){
       delay(200);
       return PB_UP;
-    } else if (digitalRead(PB_DOWN) == LOW){
+    }
+
+    else if (digitalRead(PB_DOWN) == LOW){
       delay(200);
       return PB_DOWN;
-    } else if (digitalRead(PB_OK) == LOW){
+    }
+    
+    else if (digitalRead(PB_OK) == LOW){
       delay(200);
       return PB_OK;
-    } else if (digitalRead(PB_CANCEL) == LOW){
+    }
+
+    else if (digitalRead(PB_CANCEL) == LOW){
       delay(200);
       return PB_CANCEL;
     }
+
     update_time();
   }
 }
@@ -209,39 +224,96 @@ void go_to_menu() {
 
     int pressed = wait_for_button_press();
     if (pressed == PB_UP){
-      current_mode = (current_mode + 1) % max_modes;
-    } else if (pressed == PB_DOWN){
-      current_mode = (current_mode - 1 + max_modes) % max_modes;
-    } else if (pressed == PB_OK){
+      delay(200);
+      current_mode += 1;
+      current_mode = current_mode % max_modes;
+    }
+
+    else if (pressed == PB_DOWN){
+      delay(200);
+      current_mode -= 1;
+      if (current_mode<0){
+        current_mode = max_modes-1;
+      }
+    }
+
+    else if (pressed == PB_OK){
+      delay(200);
       Serial.println(current_mode);
       run_mode(current_mode);
-    } else if (pressed == PB_CANCEL){
+    }
+
+    else if (pressed == PB_CANCEL){
+      delay(200);
       break;
     }
   }
 }
 
 void set_time() {
+
   int temp_hour = hours;
   while(true) {
     display.clearDisplay();
     print_line("Enter hour: " + String(temp_hour), 0, 0, 2);
+
     int pressed = wait_for_button_press();
-    if (pressed == PB_UP) temp_hour = (temp_hour + 1) % 24;
-    else if (pressed == PB_DOWN) temp_hour = (temp_hour - 1 + 24) % 24;
-    else if (pressed == PB_OK) { hours = temp_hour; break; }
-    else if (pressed == PB_CANCEL) break;
+    if (pressed == PB_UP) {
+      delay(200);
+      temp_hour += 1;
+      temp_hour = temp_hour % 24;
+    }
+
+    else if (pressed == PB_DOWN) {
+      delay(200);
+      temp_hour -= 1;
+      if (temp_hour < 0){
+        temp_hour = 23;
+      }
+    }
+
+    else if (pressed == PB_OK){
+      delay(200);
+      hours = temp_hour;
+      break;
+    }
+
+    else if (pressed == PB_CANCEL){
+      delay(200);
+      break;
+    }
   }
 
   int temp_minute = minutes;
   while(true) {
     display.clearDisplay();
     print_line("Enter minute: " + String(temp_minute), 0, 0, 2);
+
     int pressed = wait_for_button_press();
-    if (pressed == PB_UP) temp_minute = (temp_minute + 1) % 60;
-    else if (pressed == PB_DOWN) temp_minute = (temp_minute - 1 + 60) % 60;
-    else if (pressed == PB_OK) { minutes = temp_minute; break; }
-    else if (pressed == PB_CANCEL) break;
+    if (pressed == PB_UP) {
+      delay(200);
+      temp_minute += 1;
+      temp_minute = temp_minute % 60;
+    }
+
+    else if (pressed == PB_DOWN) {
+      delay(200);
+      temp_minute -= 1;
+      if (temp_minute < 0){
+        temp_minute = 59;
+      }
+    }
+
+    else if (pressed == PB_OK){
+      delay(200);
+      minutes = temp_minute;
+      break;
+    }
+
+    else if (pressed == PB_CANCEL){
+      delay(200);
+      break;
+    }
   }
 
   display.clearDisplay();
@@ -250,59 +322,187 @@ void set_time() {
 }
 
 void set_alarm(int alarm){
+
   int temp_hour = alarm_hours[alarm];
   while(true) {
     display.clearDisplay();
     print_line("Enter hour: " + String(temp_hour), 0, 0, 2);
+
     int pressed = wait_for_button_press();
-    if (pressed == PB_UP) temp_hour = (temp_hour + 1) % 24;
-    else if (pressed == PB_DOWN) temp_hour = (temp_hour - 1 + 24) % 24;
-    else if (pressed == PB_OK) { alarm_hours[alarm] = temp_hour; break; }
-    else if (pressed == PB_CANCEL) break;
+    if (pressed == PB_UP) {
+      delay(200);
+      temp_hour += 1;
+      temp_hour = temp_hour % 24;
+    }
+
+    else if (pressed == PB_DOWN) {
+      delay(200);
+      temp_hour -= 1;
+      if (temp_hour < 0){
+        temp_hour = 23;
+      }
+    }
+
+    else if (pressed == PB_OK){
+      delay(200);
+      alarm_hours[alarm] = temp_hour;
+      break;
+    }
+
+    else if (pressed == PB_CANCEL){
+      delay(200);
+      break;
+    }
   }
 
   int temp_minute = alarm_minutes[alarm];
   while(true) {
     display.clearDisplay();
     print_line("Enter minute: " + String(temp_minute), 0, 0, 2);
+
     int pressed = wait_for_button_press();
-    if (pressed == PB_UP) temp_minute = (temp_minute + 1) % 60;
-    else if (pressed == PB_DOWN) temp_minute = (temp_minute - 1 + 60) % 60;
-    else if (pressed == PB_OK) { alarm_minutes[alarm] = temp_minute; break; }
-    else if (pressed == PB_CANCEL) break;
+    if (pressed == PB_UP) {
+      delay(200);
+      temp_minute += 1;
+      temp_minute = temp_minute % 60;
+    }
+
+    else if (pressed == PB_DOWN) {
+      delay(200);
+      temp_minute -= 1;
+      if (temp_minute < 0){
+        temp_minute = 59;
+      }
+    }
+
+    else if (pressed == PB_OK){
+      delay(200);
+      alarm_minutes[alarm] = temp_minute;
+      break;
+    }
+
+    else if (pressed == PB_CANCEL){
+      delay(200);
+      break;
+    }
   }
 
   display.clearDisplay();
   print_line("Alarm is set", 0, 0, 2);
   delay(1000);
+
 }
 
 void run_mode(int mode) {
   if (mode == 0){
     set_time();
-  } else if (mode == 1 || mode == 2){
+  }
+
+  if (mode == 1 || mode == 2){
     set_alarm(mode - 1);
-  } else if (mode == 3){
+  }
+
+  else if (mode == 3){
     alarm_enabled = false;
+  }
+
+  else if (mode == 4){
+    set_timezone();
   }
 }
 
 void check_temp(){
   TempAndHumidity data = dhtSensor.getTempAndHumidity();
-
   if (data.temperature > 35){
     display.clearDisplay();
     print_line("TEMP HIGH", 0, 40, 1);
-  } else if (data.temperature < 25){
+  }
+  else if (data.temperature < 25){
     display.clearDisplay();
     print_line("TEMP LOW", 0, 40, 1);
   }
-
+  
   if (data.humidity > 40){
     display.clearDisplay();
     print_line("HUMIDITY HIGH", 0, 50, 1);
-  } else if (data.humidity < 20){
+  }
+  else if (data.humidity < 20){
     display.clearDisplay();
     print_line("HUMIDITY LOW", 0, 50, 1);
   }
+
 }
+
+void set_timezone() {
+  int sign = (UTC_OFFSET >= 0) ? 1 : -1;
+  int int_part = abs((int)UTC_OFFSET);
+  float decimal_part = abs(UTC_OFFSET) - int_part;
+
+  int decimal_step = (int)(decimal_part * 100); // example: 0.5 → 50
+
+  int current_step = 0;
+  String steps[] = {"Sign", "Hour", "Decimal"};
+
+  while (true) {
+    display.clearDisplay();
+    String line = "UTC Offset: ";
+    if (sign < 0) line += "-"; else line += "+";
+    line += String(int_part);
+    line += ".";
+    if (decimal_step < 10) line += "0"; // pad 0 if needed
+    line += String(decimal_step);
+
+    print_line(line, 0, 0, 2);
+    print_line("Setting: " + steps[current_step], 0, 30, 1);
+
+    int pressed = wait_for_button_press();
+
+    if (pressed == PB_UP) {
+      if (current_step == 0) {
+        sign *= -1;
+      } else if (current_step == 1) {
+        int_part = (int_part + 1) % 15;  // UTC range is -14 to +14
+      } else if (current_step == 2) {
+        decimal_step += 25;
+        if (decimal_step > 75) decimal_step = 0;
+      }
+    }
+
+    else if (pressed == PB_DOWN) {
+      if (current_step == 0) {
+        sign *= -1;
+      } else if (current_step == 1) {
+        int_part -= 1;
+        if (int_part < 0) int_part = 14;
+      } else if (current_step == 2) {
+        decimal_step -= 25;
+        if (decimal_step < 0) decimal_step = 75;
+      }
+    }
+
+    else if (pressed == PB_OK) {
+      current_step++;
+      if (current_step >= 3) {
+        float final_offset = sign * (int_part + (decimal_step / 100.0));
+        UTC_OFFSET = final_offset;
+
+        configTime((int)(UTC_OFFSET * 3600), UTC_OFFSET_DST, NTP_SERVER);
+
+        display.clearDisplay();
+        struct tm timeinfo;
+        if (!getLocalTime(&timeinfo)) {
+          print_line("Time Sync Failed", 0, 0, 1);
+        } else {
+          print_line("Timezone Set", 0, 0, 2);
+        }
+        delay(1000);
+        break;
+      }
+    }
+
+    else if (pressed == PB_CANCEL) {
+      break;
+    }
+  }
+}
+
